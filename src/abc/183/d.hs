5b4cdf86a773f3da6f32436f8f5f1e99ab26d4e9
Takanori Ikeda <takanori.ikeda@toracoya.com>
2020-12-06 22:53:58 +0900

    Solve abc-183d

--- src/abc/183/d.hs
+++ src/abc/183/d.hs
@@ -4,7 +4,7 @@ import Control.Monad
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as C
 import qualified Data.Array as A
-import qualified Data.Array.IO as IO
+import qualified Data.Array.IO      as IO
 import qualified Data.Array.Unboxed as UA
 import Data.List
 import Data.Maybe
@@ -12,11 +12,26 @@ import qualified Data.Vector.Unboxed.Mutable as VUM
 
 main :: IO ()
 main = do
-  putStrLn $ show $ solve
+  [n,w] <- readIntList
+  putStrLn =<< (\r -> if r then "Yes" else "No") <$> solve n w
 
-solve :: Int
-solve = undefined
-
+solve :: Int -> Int -> IO Bool
+solve n w = do
+  let max = 2*10^5
+  table <- VUM.replicate (max+1) 0
+  replicateM n $ do
+    [s,t,p] <- readIntList
+    VUM.modify table (+p) s
+    VUM.modify table (+(-p)) t
+  judge table 0 max w 0
+  where
+    judge table i max w acc =
+      if i > max then return True
+      else do
+        v <- VUM.read table i
+        let acc' = acc + v
+        if acc' > w then return False
+        else judge table (i+1) max w acc'
 
 readInt :: IO Int
 readInt = toInt <$> C.getLine
