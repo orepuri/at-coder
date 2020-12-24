424fc7aca11dc2cce8bfeb7887e71d273cc55e10
Takanori Ikeda <takanori.ikeda@toracoya.com>
2020-12-25 00:07:21 +0900

    [WIP] abc-007/c

--- src/abc/7/c.hs
+++ src/abc/7/c.hs
@@ -1,4 +1,5 @@
 import Control.Monad
+import qualified Data.Array.IO as AI
 import qualified Data.ByteString.Char8 as C
 import qualified Data.Vector as V
 import Data.Maybe ( fromJust )
@@ -9,10 +10,23 @@ main = do
   [sy,sx] <- readIntList
   [gy,gx] <- readIntList
   stage <- V.fromList <$> replicateM r C.getLine
-  print $ solve r c sy sx gy gx stage
+  dp <- AI.newArray ((1,1), (r,c)) -1
+  solve r c gy gx [(sy,sx)] stage dp
+  print =<< AI.readArray dp (r,c)
 
-solve :: Int -> Int -> Int -> Int -> Int -> Int -> V.Vector C.ByteString -> Int
-solve r c sy sx gy gx stage = 1
+solve :: Int -> Int -> Int -> Int -> [(Int,Int)] -> V.Vector C.ByteString -> AI.IOUArray (Int,Int) Int -> IO ()
+solve r c gy gx [] stage dp = pure ()
+solve r c gy gx (p:ps) stage dp = do
+  nexts <- filterM (\(x, y) -> do
+    d <- AI.readArray dp (x,y)
+    pure $ d /= -1 ) $ neighbors p
+  pure ()
+  where ns = 1
+    
+    -- C.index (stage V.! x) y /= '*' && ) $ neighbors p
+
+neighbors :: (Int,Int) -> [(Int,Int)]
+neighbors (r,c) = [(r-1,c),(r,c-1),(r+1,c),(r,c+1)]
 
 readIntList :: IO [Int]
 readIntList = map toInt . C.words <$> C.getLine
